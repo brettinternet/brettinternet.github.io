3a0dca8c0d9d89d2bc5fc0d7178ea0b0cc20e4fc
brettinternet <brettinternet@gmail.com>
2019-10-06 16:23:49 -0600

    upgrade packages, add feed

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -51,7 +51,7 @@ class BlogPostTemplate extends React.Component {
             {tags &&
               tags.length &&
               tags.map(tag => (
-                <A key={tag} to={`/tags/${kebabCase(tag)}`}>
+                <A key={tag} to={`/tags/${ kebabCase(tag) }`}>
                   <Tag themed mRight="0.5rem">
                     {tag}
                   </Tag>
@@ -68,13 +68,13 @@ class BlogPostTemplate extends React.Component {
               >
                 >
               </Dot>
-              <A href={`${siteRepo}/edit/source/content/blog${slug}index.md`}>
+              <A href={`${ siteRepo }/edit/source/content/blog${ slug }index.md`}>
                 Edit on GitHub
               </A>
 
               <Dot>·</Dot>
               <A
-                href={`https://twitter.com/home?status=${location.href}`}
+                href={`https://twitter.com/home?status=${ location.href }`}
                 title="Tweet post"
               >
                 <TwitterSvg height="12" />
@@ -84,22 +84,22 @@ class BlogPostTemplate extends React.Component {
             <NavButton
               buttons={[
                 ...(previous
-                  ? {
-                      dir: "backward",
-                      rel: "prev",
-                      to: (postBasePath || "") + previous.fields.slug,
-                      name: previous.frontmatter.title,
-                      css: `font-size: 13px;`,
-                    }
+                  ? [{
+                    dir: "backward",
+                    rel: "prev",
+                    to: (postBasePath || "") + previous.fields.slug,
+                    name: previous.frontmatter.title,
+                    css: `font-size: 13px;`,
+                  }]
                   : []),
                 ...(next
-                  ? {
-                      dir: "forward",
-                      rel: "next",
-                      to: (postBasePath || "") + next.fields.slug,
-                      name: next.frontmatter.title,
-                      css: `font-size: 13px;`,
-                    }
+                  ? [{
+                    dir: "forward",
+                    rel: "next",
+                    to: (postBasePath || "") + next.fields.slug,
+                    name: next.frontmatter.title,
+                    css: `font-size: 13px;`,
+                  }]
                   : []),
               ]}
             />
@@ -159,10 +159,10 @@ const Title = styled.h1`
 `
 
 const Body = styled.div`
-  ${serifFont};
+  ${serifFont };
 
   a {
-    color: ${props => props.theme.themePrimary};
+    color: ${props => props.theme.themePrimary };
 
     &:hover {
       text-decoration: underline;

799d0fef4b3bc730a83a0c98f39a717957597dd0
brett <20871604+brettinternet@users.noreply.github.com>
2025-05-23 10:24:40 -0600

    --wip-- [skipci]

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
-module.exports = {
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+/** @type {import('postcss-load-config').Config} */
+const config = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    '@tailwindcss/postcss': {
+      base: join(__dirname, '../'),
+    },
   },
 }
+
+export default config
